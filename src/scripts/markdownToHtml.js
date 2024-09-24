82e189b27b506aef87e62198949ccf2b9a065d6d
M-Mikos <mikos.marcin.m@gmail.com>
2024-09-24 10:38:57 +0200

    Add support for parsing tables from markdown

--- src/scripts/markdownToHtml.js
+++ src/scripts/markdownToHtml.js
@@ -1,7 +1,11 @@
 import { remark } from "remark";
 import html from "remark-html";
+import gfm from "remark-gfm";
 
 export default async function markdownToHtml(markdown) {
-  const result = await remark().use(html).process(markdown);
+  const result = await remark()
+    .use(html)
+    .use(gfm)
+    .process(markdown);
   return result.toString();
 }
